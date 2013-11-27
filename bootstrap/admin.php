a09f77c710a998dc355362cd77f1debe0b36efc4
khumaini <khumaini06@gmail.com>
2013-11-27 23:18:37 +0700

    Tugas_Botsrap
    Tugas_Botsrap

---
+++ bootstrap/admin.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>:: Khumaini Website ::</title>
+<link href="../config/adminstyle.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+  <div id="content">
+		<h2>Login</h2>
+        <form method="POST" action="media.php">
+<table>
+<tr><td>Username</td><td> : <input type="text" name="username" username="khum"></td></tr>
+<tr><td>Password</td><td> : <input type="password" name="password" password="khum"></td></tr>
+<tr><td colspan="2"><input type="submit" value="Login"></td></tr>
+</table>
+</form>
+
+<p>&nbsp;</p>
+  </div>
+	<div id="footer">
+		Copyright @2013 Khumaini Websites
+	</div>
+</div>
+</body>
+</html>
---
+++ bootstrap/coba.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>:: Khumaini Websites ::</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Home</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Profil</a></li>
+      <li><a href="#">Album</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Download <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Game</a></li>
+          <li><a href="#">Anti Virus</a></li>
+          <li><a href="#">Animasi</a></li>
+          <li><a href="#">Music</a></li>
+        </ul>
+      </li>
+	  <li><a href="admin.php">Login</a></li>
+    </ul>
+    <form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search"> 
+      </div>
+      <button type="submit" class="btn btn-default">Cari</button>
+    </form>
+  </div><!-- /.navbar-collapse -->
+</nav>
+    <h1>Assalamualaikum!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
